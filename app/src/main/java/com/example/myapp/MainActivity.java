63e313f5d07cc615e002900cddbce4436394a220
ASUS <shelewa1@smail.ucas.edu.ps>
2024-11-27 11:04:10 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapp.databinding.ActivityMainBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity{
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+String datajson = {
+        "book":{
+            "imgURL": "https://www.chemicals.co.uk/blog/what-do-vv-ww-wv-mean",
+                    "titel": "Hamlet",
+                    "author": "Shakespeare",
+                    "isPublished": "ture",
+                    "puplisher": {
+                "name": "simon",
+                        "location":"londen"
+            }
+
+
+
+        }
+}
+
+
+    }
+
+
+}
